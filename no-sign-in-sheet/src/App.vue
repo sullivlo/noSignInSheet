ed86ff969bb7603b765487989a06b80a48c66757
Jacob Walton <waltojac@mail.gvsu.edu>
2018-11-18 15:39:29 -0500

    Base Vue app made.

---
+++ no-sign-in-sheet/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+
+      
+      <v-toolbar-title class="headline text-uppercase">
+        <router-link to="/" tag="span" exact>
+            <a style="text-decoration: none">No Sign-in Sheet</a>
+        </router-link>
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-menu offset-y open-on-hover>
+        <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
+        <v-list>
+              <v-list-tile
+                v-for="(item, i) in items"
+                :key="i"
+                @click="menuClicked(item)"
+              >
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              </v-list-tile>
+            </v-list>
+      </v-menu>
+      
+    </v-toolbar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Home from "./views/Home";
+export default {
+  name: "App",
+  components: {
+    Home
+  },
+  data() {
+    return {
+      //
+      items: [
+        { title: "Settings", component: "settings" },
+        { title: "Log Out", component: "logOut" }
+      ]
+    };
+  },
+  methods: {
+    menuClicked(key) {
+      console.log(key.title);
+      this.$router.push({ name: key.component });
+    }
+  }
+};
+</script>
