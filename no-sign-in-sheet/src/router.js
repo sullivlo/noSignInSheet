19945802a51892672da84079b8eaa8023d138328
Jacob Walton <waltojac@mail.gvsu.edu>
2018-11-21 16:36:10 -0500

    Updated production path.

--- no-sign-in-sheet/src/router.js
+++ no-sign-in-sheet/src/router.js
@@ -8,9 +8,7 @@ import Login from "./views/Login.vue";
 Vue.use(Router);
 
 const router = new Router({
-  mode: "history",
-
-  base: process.env.BASE_URL,
+  mode: "hash",
   routes: [
     {
       path: "/login",
