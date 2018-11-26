b2b0669d5dd90efc9cb961087f23b9243031f1c4
Jacob Walton <waltojac@mail.gvsu.edu>
2018-11-26 14:34:04 -0500

    Update production.

--- no-sign-in-sheet/vue.config.js
+++ no-sign-in-sheet/vue.config.js
@@ -1,6 +1,5 @@
 // vue.config.js
 module.exports = {
-    // options...
     baseUrl: process.env.NODE_ENV === 'production'
         ? '/~waltojac/noSignInSheet/no-sign-in-sheet/dist/'
         : '/'
