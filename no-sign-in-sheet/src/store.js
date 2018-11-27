401ccaee6626de3ed16ec95ef95f6837a1c6d8d4
Jacob Walton <waltojac@mail.gvsu.edu>
2018-11-27 15:16:46 -0500

    Update store and grab firebase root refs in store.

--- no-sign-in-sheet/src/store.js
+++ no-sign-in-sheet/src/store.js
@@ -6,12 +6,18 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     uname: null,
-    email: null
+    email: null,
+    rootRef: null,
+    classRoot: null
   },
   mutations: {
     login(state, payload) {
       state.uname = payload.name;
       state.email = payload.email;
+    },
+    dbSetup(state, payload) {
+      state.rootRef = payload.root;
+      state.classRoot = payload.class;
     }
   },
   actions: {}
--- no-sign-in-sheet/src/views/Home.vue
+++ no-sign-in-sheet/src/views/Home.vue
@@ -1,9 +1,8 @@
 <template>
   <v-container>
     <v-layout row>
-      <h1>Home</h1>
       <v-label>
-        <p>{{ this.uname }}</p>
+        <h1>{{ this.$store.state.uname }}'s Home</h1>
       </v-label>
     </v-layout>
   </v-container>
@@ -15,23 +14,26 @@ import firebase from "firebase";
 export default {
   name: "App",
   created: function() {
-    var user = firebase.auth().currentUser; 
+    var user = firebase.auth().currentUser;
     // notice that a Vuex mutation cannot contain more than 1 payload parameter.
-    this.$store.commit('login', { name: user.displayName, email: user.email});
+    this.$store.commit("login", { name: user.displayName, email: user.email });
     console.log(this.$store.state.uname);
     console.log(this.$store.state.email);
-
+    this.$store.commit("dbSetup", {
+      root: firebase.database().ref(),
+      class: firebase
+        .database()
+        .ref()
+        .child("classes")
+    });
   },
   data() {
     return {
-      uname: ""
-  };
+      uname: "",
+      rootRef: null
+    };
   },
-  methods: {
-    
-  }
-
-}
-
+  methods: {}
+};
 </script>
 
