2f5590fb1c14360b99465ea93fa0bae3e2a1f6bd
Jacob Walton <waltojac@mail.gvsu.edu>
2018-11-30 12:54:02 -0500

    Updated student class list.

--- no-sign-in-sheet/src/components/Student.vue
+++ no-sign-in-sheet/src/components/Student.vue
@@ -1,43 +1,85 @@
 <template>
   <v-container>
-    <v-layout row justify-center pb-4>
-      <v-card dark>
-        <v-card-title>
-          <h2>Join a Class:</h2>
-          <v-spacer></v-spacer>
-          <v-text-field
-            v-model="search"
-            append-icon="search"
-            label="Search"
-            single-line
-            hide-details
-          ></v-text-field>
-        </v-card-title>
-        <v-divider dark/>
-        <v-data-table
-          :headers="this.headers"
-          :items="this.notClasses"
-          :search="search"
-          class="elevation-1"
-          dark
-          :rows-per-page-items="[ 5, 7, 10, 15, { text: 'All', value: -1 } ]"
-          pagination.sync="{ rowsPerPage: 7 }"
-        >
-          <template slot="items" slot-scope="props">
-            <td>{{ props.item.name }}</td>
-            <td>{{ props.item.teacher }}</td>
-            <td>
-              <v-btn @click="addStudent(props.item['.key'])">Join Class</v-btn>
-            </td>
-          </template>
-          <v-alert
-            slot="no-results"
-            :value="true"
-            color="error"
-            icon="warning"
-          >Your search for "{{ search }}" found no results.</v-alert>
-        </v-data-table>
-      </v-card>
+    <v-layout row>
+      <v-layout column pb-4>
+        <v-card dark>
+          <v-card-title>
+            <h2>Join a Class:</h2>
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="search"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+          <v-divider dark/>
+          <v-data-table
+            :headers="this.headers"
+            :items="this.notClasses"
+            :search="search"
+            class="elevation-1"
+            dark
+            :rows-per-page-items="[ 7, 10, 15, { text: 'All', value: -1 } ]"
+            pagination.sync="{ rowsPerPage: 7 }"
+          >
+            <template slot="items" slot-scope="props">
+              <td>{{ props.item.name }}</td>
+              <td>{{ props.item.teacher }}</td>
+              <td>
+                <v-btn @click="addStudent(props.item['.key'])">Join Class</v-btn>
+              </td>
+            </template>
+            <v-alert
+              slot="no-results"
+              :value="true"
+              color="error"
+              icon="warning"
+            >Your search for "{{ search }}" found no results.</v-alert>
+          </v-data-table>
+        </v-card>
+      </v-layout>
+      <v-spacer vertical/>
+      <v-layout column pb-4>
+        <v-card dark>
+          <v-card-title>
+            <h2>My Classes:</h2>
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="search"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-card-title>
+          <v-divider dark/>
+          <v-data-table
+            :headers="this.headers"
+            :items="this.myClasses"
+            :search="search"
+            class="elevation-1"
+            dark
+            :rows-per-page-items="[ 7, 10, 15, { text: 'All', value: -1 } ]"
+            pagination.sync="{ rowsPerPage: 7 }"
+          >
+            <template slot="items" slot-scope="props">
+              <td>{{ props.item.name }}</td>
+              <td>{{ props.item.teacher }}</td>
+              <td>
+                <v-btn @click="joinSession(props.item['.key'])">Join Session</v-btn>
+              </td>
+            </template>
+            <v-alert
+              slot="no-results"
+              :value="true"
+              color="error"
+              icon="warning"
+            >Your search for "{{ search }}" found no results.</v-alert>
+          </v-data-table>
+        </v-card>
+      </v-layout>
     </v-layout>
   </v-container>
 </template>
@@ -98,23 +140,49 @@ export default {
         });
       this.createClasses();
     },
+    joinSession(id) {
+      var myTime = new Date().toLocaleString();
+      const e = this.$store.state.email;
+      const u = this.$store.state.uname;
+      firebase
+        .database()
+        .ref()
+        .child(`classes/${id}/sessions`)
+        .orderByChild("date")
+        .limitToLast(1)
+        .once("child_added", function(snapshot) {
+          console.log(snapshot.key);
+          firebase
+            .database()
+            .ref()
+            .child(`classes/${id}/sessions/${snapshot.key}/attendees`)
+            .push()
+            .set({
+              email: e,
+              name: u,
+              present: true
+            });
+        });
+
+      this.createClasses();
+    },
     createClasses() {
       //find classes not currently entrolled
       //loop through all this.allCasses
       //  check if current user is in the student list
 
-      console.log("All Classes:");
-      console.log(this.allClasses);
-      console.log(typeof this.allClasses);
+      // console.log("All Classes:");
+      // console.log(this.allClasses);
+      // console.log(typeof this.allClasses);
 
       var tmpMyClasses = [];
       var tmpNotMyClasses = [];
 
-      console.log("Before class loop");
+      // console.log("Before class loop");
 
       //loop thru classes
       for (var currClass in this.allClasses) {
-        console.log("In class loop");
+        // console.log("In class loop");
         var studList = this.allClasses[currClass].students;
         var inClass = false;
 
@@ -125,13 +193,13 @@ export default {
           for (var stud in studList) {
             var stuEmail = studList[stud].email;
             if (stuEmail == this.$store.state.email) {
-              console.log("I am in this class yee haw ");
+              // console.log("I am in this class yee haw ");
               //console.log(this.allClasses[currClass]);
               //add classes to myClasses
               inClass = true;
               //tmpMyClasses.push(this.allClasses[currClass]);
             } else {
-              console.log("I am not in this class darn it  ");
+              // console.log("I am not in this class darn it  ");
               //console.log(this.allClasses[currClass]);
               //add classes to not myClasses
               //tmpNotMyClasses.push(this.allClasses[currClass]);
@@ -145,11 +213,7 @@ export default {
           tmpNotMyClasses.push(this.allClasses[currClass]);
         }
       }
-      console.log(typeof tmpNotMyClasses);
-      console.log(tmpNotMyClasses);
 
-      console.log(typeof tmpMyClasses);
-      console.log(tmpMyClasses);
       this.myClasses = tmpMyClasses;
       this.notClasses = tmpNotMyClasses;
       //return { myClasses: tmpMyClasses, notClasses: tmpNotMyClasses };
